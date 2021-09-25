dc1ce04efc28935fb8d5cb05df65259a8733fc4e
czs0059 <czs0059@auburn.edu>
2021-09-25 15:12:31 -0500

    deleted code

--- gitwalk.R
+++ gitwalk.R
@@ -9,13 +9,3 @@ r=3
 r^2
 pi*r^2
 
-##the below code creates everal objects with specified values these objects are vectors with one element or one dimension
-a=2+2
-a
-b=8-5
-b
-d=a*b
-d
-r=3
-r^2
-pi*r^2
